9d6268eb3d0a3415759e687b6163856fa5b8264b
Max Schmitt <max98tonton@gmail.com>
2020-01-02 02:24:27 +0100

    fixed testfile

--- TestScene.gd
+++ TestScene.gd
@@ -6,8 +6,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#$DialogueBox.talk(["Krass", "Also... so irgendwie...", "Also... s234o irgendwie...", "A12432535646lso... so irgendwie..."])
 	pass
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_cancel"):
+		$DialogueBox.talk(["Krass", "Also... so irgendwie...", "Also... s234o irgendwie...", "A12432535646lso... so irgendwie..."])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
