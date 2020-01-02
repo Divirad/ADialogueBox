a2317957fb48a8a65538ee1473faea2a9d054ace
Max Schmitt <max98tonton@gmail.com>
2020-01-02 03:03:44 +0100

    updated testscene

--- TestScene.gd
+++ TestScene.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-func _ready():
-	$DialogueBox.talk(["Lorem ipsum dolor sit amet,", 
-	"consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,",
-	 "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
-	" Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "])
+func _input(event):
+	if Input.is_action_just_pressed("ui_down"):
+		$DialogueBox.talk(["Lorem ipsum dolor sit amet,", 
+		"consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,",
+	 	"sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+		" Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "])
